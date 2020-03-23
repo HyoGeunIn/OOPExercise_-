239b9e57e641fc14621a38f4bda04ae02deb1b97
인효근 <@DESKTOP-A1M0R0N>
2020-03-23 20:24:34 +0900

    멤버베뉴까지

--- src/member/model/vo/Member.java
+++ src/member/model/vo/Member.java
@@ -26,6 +26,42 @@ public class Member {
 	public Member(int age , char gender) {
 		
 	}
+	public String getUserId() {
+		return userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 
 }
